cf78aeb90fb4e044d055f963dd036abc00b3fa6a
aak2510 <akanda2500@gmail.com>
2024-03-08 23:05:31 +0000

    Updating repo with working with classes work sheet
    Restructuring repo layout

---
+++ ChallengeProjectOneApp/ChallengeProjectOne/Program.cs
@@ -0,0 +1,97 @@
+﻿// Constant username and password, these are set and we don't want them to change throught the program
+const string username = "anish";
+const string password = "Peregrine1";
+
+// Check that log-in credentials are correct, if they are then proceed, else repeat until they enter the correct username and password.
+bool inputSuccess = false;
+/* The log in menu will DEFINETLY be displayed once and we only need to check the condition after an input. 
+ * It doesn't make sense to check before the user has even had a change to input their credentials. 
+ * Thus refactoring this to a do while is a viable option.
+ */
+do
+{
+    // Ask user for username, not case sensitive, so we convert to the same case
+    Console.Write("Please enter your username: ");
+    string? inputUsername = Console.ReadLine().ToLower().Trim();
+    // Password IS case sensitive, so we don't change case
+    Console.Write("Please enter your password: ");
+    string? inputPassword = Console.ReadLine().Trim();
+
+    // Only leave the log in page if BOTH username and password are the same
+    if (inputUsername == username && inputPassword == password)
+    {
+        inputSuccess = true;
+        Console.WriteLine();
+    }
+    else
+    {
+        // Message to let user know that input is incorrect
+        Console.WriteLine("Username or password is incorrect, please try again.\n");
+    }
+} while (!inputSuccess);
+
+
+
+
+// menu choices using switch statement, within while loop to keep repeating menu until user selects exit menu
+/*
+ * Changed this to a do while as it is more suitable. Do while loop run at least once and this Menu needs to be display At least once after a successful log in attempt.
+ * Thus, this refactoring seems apprpriate.
+ */
+bool quitMenu = false;
+do
+{
+    //Single writeline statement as it is more efficient than printing multiple.
+    Console.WriteLine("""
+    -- Main menu --
+    1. create a new member
+    2. view members
+    3. delete members
+    x. exit system   
+    """);
+
+    //using a try statement incase user enters an incorrect input (null or letters)
+    int? menuChoice = 0;
+    string? menuChoiceString;
+    Console.Write("Please enter a the menu option number: ");
+    try
+    {
+        //if the user entered x, then quit the program
+        menuChoiceString = Console.ReadLine();
+        if (menuChoiceString == "x")
+        {
+            Console.WriteLine("Exiting the sysem, thank you :)\n\n");
+            quitMenu = true;
+            break;
+        }
+        else
+        {
+            // If the user hasn't enetered 'x', then we try to parse the value as an integer. Here we are assuming that the user is inputting a menu option other than 'x'.
+            // If the value can't be parsed, the catch block will be executed as an exception will be thrown
+            menuChoice = int.Parse(menuChoiceString);
+        }
+
+        // If a number was entered then run the corresponding menu item, or ask again if incorrect input
+        switch (menuChoice)
+        {
+            case 1:
+                Console.WriteLine("Created a new member...\n\n");
+                break;
+            case 2:
+                Console.WriteLine("Displaying all members...\n\n");
+                break;
+            case 3:
+                Console.WriteLine("Deleting all members...\n\n");
+                break;
+            default:
+                // Erroneous input will be caught by the catch clause
+                throw new Exception();
+        }
+
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("You have entered an invalid input, please try again. Returning to main menu...\n\n");
+    }
+
+} while (!quitMenu);
---
+++ WorkingWithClassesApp/WorkingWithClasses/Animal.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithClasses
+{
+    //class Animal
+    //{
+
+    //    // The get and set means that we can get and set, regardless of it being private or public. If its private then we can only set them using methods, as normal.
+    //    private string Name { get; set; }
+    //    public string? TypeOfAnimal { get; set; }
+    //    private int Age { get;  set; }
+    //    public string Colour { get; set; }
+    //    public decimal Price { get; set; }
+    //    public decimal Cost { get; set; }
+
+    //    public Animal(string name, string typeOfAnimal, int age, string colour, decimal price, decimal cost)
+    //    {
+    //        Name = name ?? throw new ArgumentNullException(nameof(name));
+    //        TypeOfAnimal = typeOfAnimal;
+    //        Age = age;
+    //        Colour = colour ?? throw new ArgumentNullException(nameof(colour));
+    //        Price = price;
+    //        Cost = cost;
+    //    }
+
+    //    public Animal()
+    //    {
+    //        Name = "No Name";
+    //        TypeOfAnimal = "No Type";
+    //        Age = 0;
+    //        Colour = "No Colour";
+    //        Price = 0m;
+    //        Cost = 0;
+    //    }
+
+    //    public string getName()
+    //    {
+    //        return Name;
+    //    }
+
+    //    public void setName(string name)
+    //    {
+    //        Name = name;
+    //    }
+
+
+    //    //public int getAge()
+    //    //{
+    //    //    return Age;
+
+    //    //}
+
+    //    //public void increaseAge()
+    //    //{
+    //    //    this.Age++;
+    //    //}
+
+    //}
+
+    class Animal  // Base class (parent) 
+    {
+        public string hi = "meow";
+        public void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal  // Derived class (child) 
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal  // Derived class (child) 
+    {
+        public void AnimalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Pig myPig = new Pig();  // Create a Pig object
+            Dog myDog = new Dog();  // Create a Dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.AnimalSound();
+            Console.WriteLine(myPig.hi);
+            Console.WriteLine(myDog.hi);
+            Console.WriteLine((myPig is Animal));
+            Console.WriteLine((myDog is Animal));
+        }
+    }
+
+}
