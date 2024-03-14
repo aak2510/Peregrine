90be2db0f3a2336e3313c0f62c4950ddaa89b4e8
aak2510 <akanda2500@gmail.com>
2024-03-14 09:51:40 +0000

    This is to clean up the repo so that git ignore can work

--- ChallengeProjectOneApp/ChallengeProjectOne/obj/Debug/net8.0/ChallengeProjectOne.AssemblyInfo.cs
+++ ChallengeProjectOneApp/ChallengeProjectOne/obj/Debug/net8.0/ChallengeProjectOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengeProjectOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e34a83d8c52e01d8cf3a28ea72d92c98f0c4ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b18738c9aad22264d77ffffbc6bc683503a8408")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengeProjectOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengeProjectOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
