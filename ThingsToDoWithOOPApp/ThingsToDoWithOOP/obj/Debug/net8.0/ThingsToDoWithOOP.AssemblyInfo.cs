b878a26025652f8110f83bac3c224d37c1687c53
aak2510 <akanda2500@gmail.com>
2024-03-25 21:18:08 +0000

    ?

--- ThingsToDoWithOOPApp/ThingsToDoWithOOP/obj/Debug/net8.0/ThingsToDoWithOOP.AssemblyInfo.cs
+++ ThingsToDoWithOOPApp/ThingsToDoWithOOP/obj/Debug/net8.0/ThingsToDoWithOOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThingsToDoWithOOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a8958118e98de64262bb82f22b501748b006f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4ce0d5adf8092bfddf194e1b3107a593db967e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThingsToDoWithOOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThingsToDoWithOOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
